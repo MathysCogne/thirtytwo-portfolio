81083b8beed0205228f1ce62ecaceec7c8790462
Mathys Cogné <mathys.cognef@gmail.com>
2023-07-07 13:21:44 +0200

    Init work

---
+++ README.md
@@ -0,0 +1,24 @@
+<h1 align="center">Portfolio Agence ThirtyTwo</h1>
+
+<div align="center">
+   Project for Mathis Petit - 07/2023
+</div>
+
+<div align="center">
+  <h3>
+    <span> | </span>
+    <a href="https://mathyscogne.github.io/thirtytwo-portfolio/">
+      Solution // IN BUILD
+    </a>
+    <span> | </span>
+  </h3>
+</div>
+
+<!-- OVERVIEW -->
+
+
+## Overview
+
+
+![screenshot](https://github.com/MathysCogne/thirtytwo-portfolio/blob/main/assets/screen-v1.png)
+!
---
+++ php/form.php
@@ -0,0 +1,45 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   // Valider les données
+   if (
+      !empty($_POST['name']) &&
+      !empty($_POST['subject']) &&
+      !empty($_POST['email']) &&
+      !empty($_POST['message'])
+   ) {
+      // Récupérer les données du formulaire et nettoyer
+      $name = htmlspecialchars($_POST['name']);
+      $subject = htmlspecialchars($_POST['subject']);
+      $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+      $message = htmlspecialchars($_POST['message']);
+
+      // Adresse email sur laquelle vous souhaitez recevoir les messages
+      $to = "contact@mathys-cognefoucault.fr";
+
+      // Préparer le contenu de l'email
+      $email_content = "Nom: $name\n";
+      $email_content .= "Sujet: $subject\n";
+      $email_content .= "Email: $email\n";
+      $email_content .= "Message:\n$message\n";
+
+      // En-têtes de l'email
+      $headers = "From: $to\r\n";
+      $headers .= "Reply-To: $email\r\n";
+      $headers .= "MIME-Version: 1.0\r\n";
+      $headers .= "Content-type: text/plain; charset=UTF-8\r\n";
+
+      // Envoyer l'email avec le protocole SMTP
+      $subject = "PORFOFOLIO - Nouveau message depuis le formulaire de contact / $name";
+
+      // Envoyer l'email avec SMTP
+      mail($to, $subject, $email_content, $headers, "-f $email");
+
+      // Envoyer une réponse au formulaire pour indiquer que le traitement a été effectué avec succès
+      http_response_code(200); // OK
+      echo "Le formulaire a été soumis avec succès.";
+   } else {
+      // Si les données sont manquantes, envoyer une réponse d'erreur
+      http_response_code(400); // Bad Request
+      echo "Veuillez remplir tous les champs du formulaire.";
+   }
+}
